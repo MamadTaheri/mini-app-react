dc2e38b4921b9edb6a0e92aa7c9efd65e85c2915
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-01 21:51:15 +0430

    create UserForm with its routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router';
+
+import UserForm from './components/UserForm/UserForm';
 
 const App = () => {
   return (
     <div>
       
+      <>
+        <Route path="/signup" component={UserForm} />
+      </>
     </div>
   );
 };
---
+++ src/components/UserForm/UserForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './UserForm.module.css'
+
+const UserForm = () => {
+    return (
+        <div>
+            <h1>User sign up</h1>   
+        </div>
+    );
+};
+
+export default UserForm;
