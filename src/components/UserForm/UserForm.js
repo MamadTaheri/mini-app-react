bc3ff033b4fc4b11e5892fb4fa8b7e5a98305d50
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-01 23:01:46 +0430

    add font and complete UserForm

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import logo from '../../images/ASROON.svg'
+
 import styles from './UserForm.module.css'
 
 const UserForm = () => {
     return (
-        <div>
-            <h1>User sign up</h1>   
+        <div className={styles.container}>
+            <img src={logo} />
+            <form className={styles.formContainer}>
+                <h3 className={styles.titr}>فرم زیر را پر کنید</h3>    
+                <div className={styles.formGroup}>
+                   <label>نام و نام خانوادگی</label>
+                   <input type="text" placeholder="نام و نام خانوادگی شما"/>
+                </div>   
+                <div className={styles.formGroup}>
+                   <label>شماره موبایل</label>
+                   <input type="text" placeholder="شماره موبایل"/>
+                </div>   
+                <div className={styles.formGroup}>
+                   <label>سن</label>
+                   <input type="text" placeholder="سن شما"/>
+                </div>   
+                <div className={styles.formGroup}>
+                   <label>ایمیل</label>
+                   <input type="text" placeholder="ایمیل شما"/>
+                </div>  
+                <button className={styles.btn}>ساخت اکانت</button> 
+            </form>   
         </div>
     );
 };
