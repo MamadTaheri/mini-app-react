e74ba47aace2fc34d054e3495b82355646a5d0bb
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-02 00:29:19 +0430

    working on UserForm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
@@ -6,12 +6,20 @@ import Navbar from "./components/Navbar/Navbar";
 import UserForm from "./components/UserForm/UserForm";
 
 const App = () => {
+
+  const [data, setData] = useState({})
+
+  const userSubmitHandler = (data) => {
+    setData(data)
+    console.log("salam")
+  }
+
   return (
     <div>
       <Navbar />
       <>
         <Switch>
-          <Route path="/signup" component={UserForm} />
+          <Route path="/signup" render={(props) => <UserForm submit={userSubmitHandler} type="create" {...props} />}/>
           <Route path="/" component={Home} />
         </Switch>
       </>
--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../../images/ASROON.svg'
 
 import styles from './UserForm.module.css'
 
-const UserForm = () => {
+const UserForm = (props) => {
+
+    const [data, setData] = useState({
+        name: "",
+        mobile: "",
+        age: "",
+        email: "",
+    })
+
+    const changeHandler = (event) => {
+        setData({...data, [event.target.name]: event.target.value})
+    }
+
+    const submitHandler = event => {
+        event.preventDefault()
+        props.submit(data)
+        props.history.push("/")
+    }
+
     return (
         <div className={styles.container}>
             <img src={logo} />
-            <form className={styles.formContainer}>
+            <form className={styles.formContainer} onSubmit={submitHandler}>
                 <h3 className={styles.titr}>فرم زیر را پر کنید</h3>    
                 <div className={styles.formGroup}>
                    <label>نام و نام خانوادگی</label>
-                   <input type="text" placeholder="نام و نام خانوادگی شما"/>
+                   <input type="text" name="name" value={data.name} onChange={changeHandler} placeholder="نام و نام خانوادگی شما" />
                 </div>   
                 <div className={styles.formGroup}>
                    <label>شماره موبایل</label>
-                   <input type="text" placeholder="شماره موبایل"/>
+                   <input type="text" name="mobile" value={data.mobile} onChange={changeHandler} placeholder="شماره موبایل" />
                 </div>   
                 <div className={styles.formGroup}>
                    <label>سن</label>
-                   <input type="text" placeholder="سن شما"/>
+                   <input type="text" name="age" value={data.age} onChange={changeHandler} placeholder="سن شما" />
                 </div>   
                 <div className={styles.formGroup}>
                    <label>ایمیل</label>
-                   <input type="text" placeholder="ایمیل شما"/>
+                   <input type="email" name="email" value={data.email} onChange={changeHandler} placeholder="ایمیل شما" />
                 </div>  
-                <button className={styles.btn}>ساخت اکانت</button> 
+                <button type="submit" className={styles.btn}>ساخت اکانت</button> 
             </form>   
         </div>
     );
