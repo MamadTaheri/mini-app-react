e70b76b10c78d7cbc96e803a76aa9df77584f6b1
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-01 22:14:12 +0430

    Add Switch to Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Route } from 'react-router';
-import Home from './components/Home/Home';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import { Route, Switch } from "react-router";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 
-import UserForm from './components/UserForm/UserForm';
+import UserForm from "./components/UserForm/UserForm";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <>
-        <Route path="/signup" component={UserForm} />
-        <Route path="/" component={Home} />
+        <Switch>
+          <Route path="/signup" component={UserForm} />
+          <Route path="/" component={Home} />
+        </Switch>
       </>
     </div>
   );
